55dda07323a46efd4fafab1f3512c85149374a6f
peknight <JKpeknight@gmail.com>
2019-04-16 11:55:53 +0800

    update pek-file port

--- docker/nginx/file/run-nginx.sh
+++ docker/nginx/file/run-nginx.sh
@@ -11,4 +11,4 @@ fi
 if [ ! -e "$conf_dir/nginx.conf" ]; then
   cp nginx.conf $conf_dir
 fi
-docker run -d --name pek-file -h pek-file -v $target_dir:/data/file -v $conf_dir/nginx.conf:/etc/nginx/nginx.conf:ro -p 8080:8080 nginx
+docker run -d --name pek-file -h pek-file -v $target_dir:/data/file -v $conf_dir/nginx.conf:/etc/nginx/nginx.conf:ro -p 8082:8082 nginx
