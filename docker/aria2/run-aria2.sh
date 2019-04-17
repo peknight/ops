0a99a88d6c134ecbd076286d4717be2263c849a0
peknight <peknight@qq.com>
2019-04-17 13:58:27 +0800

    update aria2 rpc-secret

--- docker/aria2/run-aria2.sh
+++ docker/aria2/run-aria2.sh
@@ -3,4 +3,12 @@ target_dir=$HOME/software/aria2/data/downloads
 if [ ! -d "$target_dir" ]; then
   mkdir -p $target_dir
 fi
-docker run -d --name pek-aria2 -h pek-aria2 -v $target_dir:/data -p 6800:6800 -p 9100:8080 pek/webui-aria2
+conf_dir=$HOME/software/aria2/etc/downloads
+if [ ! -d "$conf_dir" ]; then
+  mkdir -p $conf_dir
+fi
+
+if [ ! -e "$conf_dir/aria2.conf" ]; then
+  cp aria2.conf $conf_dir/
+fi
+docker run -d --name pek-aria2 -h pek-aria2 -v $conf_dir/aria2.conf:/home/dummy/.aria2/aria2.conf -v $target_dir:/data -p 6800:6800 -p 9100:8080 pek/webui-aria2
