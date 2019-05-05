f8b77924af26295f466c01472f9c3f87232919eb
peknight <peknight@qq.com>
2019-05-05 12:05:25 +0800

    update aria2 tracker conf

--- docker/aria2/run-aria2.sh
+++ docker/aria2/run-aria2.sh
@@ -11,4 +11,6 @@ fi
 if [ ! -e "$conf_dir/aria2.conf" ]; then
   cp aria2.conf $conf_dir/
 fi
+tracker=$(curl -sSL 'https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best.txt' | sed '/^\s*$/d' | tee tracker | tr '\n' ',' | sed 's/,$//')
+sed -i "s!^\(bt-tracker=\).*!\1${tracker}!" $conf_dir/aria2.conf
 docker run -d --name pek-aria2 -h pek-aria2 -v $conf_dir/aria2.conf:/home/dummy/.aria2/aria2.conf -v $target_dir:/data -p 6800:6800 -p 9100:8080 pek/webui-aria2
