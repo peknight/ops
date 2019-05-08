4783cd3e9903ed2961257db0cbcc46dac82c3622
peknight <peknight@qq.com>
2019-05-08 15:48:23 +0800

    update switchyomega pac config

---
+++ web/pac/gfwlist.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget -O gfwlist.txt https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt
+wget -O GFWList.bak https://raw.githubusercontent.com/wiki/FelisCatus/SwitchyOmega/GFWList.bak
