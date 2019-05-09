fc8a8ad6d5c84104ad85c85f2dabcdd1bb3878ef
peknight <JKpeknight@gmail.com>
2019-05-09 03:23:01 +0000

    update network for docker ssserver

--- docker/shadowsocks/server/run-ssserver.sh
+++ docker/shadowsocks/server/run-ssserver.sh
@@ -6,4 +6,4 @@ fi
 if [ ! -e "$conf_dir/shadowsocks.json" ]; then
   cp shadowsocks.json $conf_dir/ 
 fi
-docker run -d --name pek-ssserver -h pek-ssserver -v $conf_dir/shadowsocks.json:/etc/shadowsocks.json -p 8388:8388 pek/shadowsocks-server
+docker run -d --name pek-ssserver -h pek-ssserver -v $conf_dir/shadowsocks.json:/etc/shadowsocks.json --net host --cap-add NET_ADMIN pek/shadowsocks-server
