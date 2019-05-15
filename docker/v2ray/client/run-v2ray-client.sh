8df4133d2ae8f04ba1a2e51588094a3f1dda60ef
peknight <JKpeknight@gmail.com>
2019-05-15 01:13:35 +0000

    update v2ray client

--- docker/v2ray/client/run-v2ray-client.sh
+++ docker/v2ray/client/run-v2ray-client.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+while getopts :u: opt
+do
+    case "$opt" in
+        u) UUID=$OPTARG;;
+        *) echo "Unknown option: $opt";;
+    esac
+done
 conf_dir=$HOME/software/v2ray/client/etc
 if [ ! -d "$conf_dir" ]; then
   mkdir -p $conf_dir
 fi
 if [ ! -e "$conf_dir/config.json" ]; then
   cp config.json $conf_dir/ 
-  sed -i "s/60ca58e9-003e-4c01-98de-c2223ae49153/$UUID/g" $conf_dir/config.json
+  if [ -n "$UUID" ]; then
+    sed -i "s/60ca58e9-003e-4c01-98de-c2223ae49153/$UUID/g" $conf_dir/config.json
+  fi
 fi
 log_dir=$HOME/software/v2ray/client/log
 if [ ! -d "$log_dir" ]; then
