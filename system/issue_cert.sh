c9e4b25cfb7f34d992c6333036c200962481d7cf
peknight <JKpeknight@gmail.com>
2019-05-17 13:09:36 +0800

    update install-cert

--- system/issue_cert.sh
+++ system/issue_cert.sh
@@ -1,4 +1,3 @@
 export Ali_Key=''
 export Ali_Secret=''
-/entry.sh --issue --dns dns_ali -d peknight.com -d *.peknight.com -d *.server.peknight.com -d *.v6.server.peknight.com -d *.ctrl.peknight.com -d *.vpn.peknight.com -d *.v6.vpn.peknight.com -d pek.ink -d *.pek.
-ink -d pek.space -d *.pek.space -k ec-256
+/entry.sh --issue --dns dns_ali -d peknight.com -d *.peknight.com -d *.server.peknight.com -d *.v6.server.peknight.com -d *.ctrl.peknight.com -d *.vpn.peknight.com -d *.v6.vpn.peknight.com -d pek.ink -d *.pek.ink -d pek.space -d *.pek.space -k ec-256
