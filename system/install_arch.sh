fcfe27821bb6e8fb24a2455d6e71807f75f5e3d2
peknight <JKpeknight@gmail.com>
2019-07-03 22:28:26 +0800

    update install arch script

--- system/install_arch.sh
+++ system/install_arch.sh
@@ -56,38 +56,91 @@ arch-chroot /mnt
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 hwclock --systohc
 
+# 安装更新
+pacman -Syu
+# 安装vim
 pacman -Sy vim
 # vim /etc/pacman.conf
 # 去除multilib相关注释
 
-# 修改sshd_config文件 修改ssh端口号 注意开通8612端口号的防火墙
-#vim /etc/ssh/sshd_config
-#Port 8612
-#PermitRootLogin no
-#PasswordAuthentication no
-
-# 安装必要的软件包
+# 安装更新
 pacman -Syu
 # 安装启动引导相关程序
 pacman -Sy intel-ucode os-prober grub efibootmgr
-# 安装windows文件系统支持
-pacman -Sy ntfs-3g
+# 安装网络相关程序
+pacman -Sy dialog wpa_supplicant networkmanager net-tools
+# 安装常用软件
+pacman -Sy bash-completion git docker sshfs screenfetch
+# 再次更新
+pacman -Syu
+
+# 配置bash-completion docker
+mkdir -p /etc/bash_completion.d/
+curl -L https://raw.githubusercontent.com/docker/machine/v0.16.0/contrib/completion/bash/docker-machine.bash -o /etc/bash_completion.d/docker-machine
+
+# 配置自启动项
+systemctl enable docker
+systemctl enable NetworkManager
+
+# 本地化
+# 去除en_US.UTF-8 zh_CN.UTF-8前的注释
+# vim /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+
+# 设置hostname
+echo "pek-pc" >> /etc/hostname
+# 设置hosts
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1 localhost" >> /etc/hosts
+echo "127.0.1.1 pek-pc" >> /etc/hosts
+
+# 配置引导程序
+grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# 设置密码
+# passwd
+
+# 添加组
+groupadd -g 612 pek
+groupadd -g 727 shared
+groupadd -g 1000 server
+groupadd -g 1001 tauriel
+
+# 添加用户
+useradd -c "peknight" -g pek -G adm,docker,shared,server -m -s /bin/bash -u 612 pek
+echo 'pek   ALL=(ALL)  NOPASSWD:ALL' | EDITOR='tee -a' visudo
+# passwd pek
+
+useradd -c "server" -g server -G adm,docker,shared  -m -s /bin/bash -u 1000 server
+echo 'server ALL=(ALL) ALL' | EDITOR='tee -a' visudo
+
+useradd -c "tauriel" -g tauriel -G adm,docker,shared,server -m -s /bin/bash -u 1001 tauriel
+echo 'tauriel ALL=(ALL) ALL' | EDITOR='tee -a' visudo
+# passwd tauriel
+
+# 退出重启
+exit
+umount -R /mnt
+reboot
+
+# root登录
+
 # 安装nvidia显卡驱动
 pacman -Sy nvidia nvidia-utils lib32-nvidia-utils
 # 或安装intel显卡驱动
 # pacman -Sy xf86-video-intel mesa lib32-mesa 
 # 安装图形界面相关程序
 pacman -Sy xorg gnome gnome-tweaks gdm wmctrl xdotool
-# 安装网络相关程序
-pacman -Sy dialog wpa_supplicant networkmanager net-tools
-# 安装蓝牙驱动
-pacman -Sy bluez
 # 安装声卡驱动
 pacman -Sy alsa lib32-alsa-plugins
+# 安装蓝牙驱动
+pacman -Sy bluez
+# 安装windows文件系统支持
+pacman -Sy ntfs-3g
 # 安装字体
 pacman -Sy ttf-liberation wqy-microhei wqy-zenhei
-# 安装常用软件
-pacman -Sy bash-completion git docker sshfs
 # 安装终端
 pacman -Sy tilix
 # 安装浏览器
@@ -102,33 +155,13 @@ pacman -Sy libreoffice-still
 pacman -Sy telegram-desktop
 # 安装steam与相关依赖
 pacman -Sy steam steam-native-runtime libpng12
-# 再次更新
-pacman -Syu
-mkdir -p /etc/bash_completion.d/
-curl -L https://raw.githubusercontent.com/docker/machine/v0.16.0/contrib/completion/bash/docker-machine.bash -o /etc/bash_completion.d/docker-machine
-systemctl enable docker
-systemctl enable NetworkManager
-systemctl enable gdm
-systemctl enable bluetooth
-systemctl enable sshd
 
-# 编辑gnome-tweaks配置，修改python3依赖
-#vim /usr/bin/gnome-tweaks
-#!/usr/bin/env python3 to #!/usr/bin/python3
-
-# 本地化
-# 去除en_US.UTF-8 zh_CN.UTF-8前的注释
-# vim /etc/locale.gen
-locale-gen
-echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-
-# 设置hostname
-echo "pek-pc" >> /etc/hostname
-
-# 设置hosts
-echo "127.0.0.1 localhost" >> /etc/hosts
-echo "::1 localhost" >> /etc/hosts
-echo "127.0.1.1 pek-pc" >> /etc/hosts
+# 配置sshd
+# 修改sshd_config文件 修改ssh端口号 注意开通8612端口号的防火墙
+#vim /etc/ssh/sshd_config
+#Port 8612
+#PermitRootLogin no
+#PasswordAuthentication no
 
 # 全局PATH变量加. /etc/profile
 #export PATH=.:$PATH
@@ -136,34 +169,17 @@ echo "127.0.1.1 pek-pc" >> /etc/hosts
 #export QT_IM_MODULE=fcitx
 #export XMODIFIERS=@im=fcitx
 
-# 设置密码
-# passwd
+# 编辑gnome-tweaks配置，修改python3依赖
+#vim /usr/bin/gnome-tweaks
+#!/usr/bin/env python3 to #!/usr/bin/python3
 
-# 配置引导程序
-grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
+# 配置自启动项
+systemctl enable gdm
+systemctl enable bluetooth
+systemctl enable sshd
 
-groupadd -g 612 pek
-groupadd -g 727 shared
-groupadd -g 1000 server
-groupadd -g 1001 tauriel
-
-useradd -c "peknight" -g pek -G adm,docker,shared,server -m -s /bin/bash -u 612 pek
-echo 'pek   ALL=(ALL)  NOPASSWD:ALL' | sudo EDITOR='tee -a' visudo
-# passwd pek
-
-useradd -c "server" -g server -G adm,docker,shared  -m -s /bin/bash -u 1000 server
-echo 'server ALL=(ALL) ALL' | sudo EDITOR='tee -a' visudo
-
-useradd -c "tauriel" -g tauriel -G adm,docker,shared,server -m -s /bin/bash -u 1001 tauriel
-echo 'tauriel ALL=(ALL) ALL' | sudo EDITOR='tee -a' visudo
-# passwd tauriel
-
-
-# 退出重启
-exit
-umount -R /mnt
-reboot
+# 切换到pek
+# su - pek
 
 # 生成密钥对
 mkdir -p .ssh
@@ -190,24 +206,30 @@ ln -s $HOME/ops/vim/vimrc $HOME/.vimrc
 
 mkdir -p software/aur
 cd software/aur
+
 # 安装yay
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 cd ../
+
 # 安装topicons-plus
 git clone https://aur.archlinux.org/gnome-shell-extension-topicons-plus.git
 cd gnome-shell-extension-topicons-plus/
 makepkg -si
 cd ../
+
 # 安装dash-to-dock
 git clone https://aur.archlinux.org/gnome-shell-extension-dash-to-dock.git
 cd gnome-shell-extension-dash-to-dock
 makepkg -si
 cd ../
+
 # 安装minecraft
 git clone https://aur.archlinux.org/minecraft-launcher.git
 cd minecraft-launcher
 makepkg -si
 cd ../
 
+# 重启
+sudo shutdown -r now
