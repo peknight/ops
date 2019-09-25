3f7404d8da39941c2956e5a752a349dfc71e8ca9
peknight <JKpeknight@gmail.com>
2019-09-25 09:51:35 +0800

    update fonts

--- font/win_fonts.sh
+++ font/win_fonts.sh
@@ -1,9 +1,5 @@
-mkdir /mnt/win10
-mount /dev/sda7 /mnt/win10
 mkdir /usr/share/fonts/windows
-cp -r /mnt/win10/Windows/Fonts/* /usr/share/fonts/windows/
-rm /usr/share/fonts/windows/*.fon
-chmod 644 /usr/share/fonts/windows/*
+cp -r $HOME/ops/font/windows/* /usr/share/fonts/windows/
 cd /usr/share/fonts/windows/
 mkfontscale
 mkfontdir
