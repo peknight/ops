f3a44d7f8939e5730c41014376719a91ec408781
peknight <JKpeknight@gmail.com>
2019-09-25 17:16:23 +0800

    update install windows script

--- system/install_windows.sh
+++ system/install_windows.sh
@@ -1,7 +1,22 @@
-# 设置USB设备为启动分区 (partition type 7, and bootable flag)
+cd $HOME/software/aur
+git clone https://aur.archlinux.org/ms-sys.git
+cd ms-sys
+makepkg -si
+sudo su -
 fdisk /dev/sdb
-# U盘格式化
-mkfs.ntfs -f /dev/sdb1
+# d 删除现有分区
+# o 创建新的DOS分区表
+# n 创建新分区
+# t 设置分区类型
+# c 分区类型设置为c: W95 FAT32 (LBA)
+# a 开启bootable标志
+# w 保存退出
+
+# 格式化
+mkfs.vfat /dev/sdb1
+# 写入mbr信息
+ms-sys -7 /dev/sdb
+
 # 挂载iso镜像文件
 mount -o loop windows10.iso /mnt/iso/win10
 # 挂载USB设备
