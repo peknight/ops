49684c54fdc7e821bec1f073985fa3de02d15633
peknight <JKpeknight@gmail.com>
2020-10-29 16:50:43 +0800

    更新postgres脚本

--- docker/postgres/run-postgres.sh
+++ docker/postgres/run-postgres.sh
@@ -8,4 +8,10 @@ if [ ! -d "$conf_dir" ]; then
   mkdir -p $conf_dir
 fi
 cp postgres.conf $conf_dir/
-docker run -d --name pek-postgres -h pek-postgres --user 612:612 -e POSTGRES_USER=pek -e POSTGRES_PASSWORD=*** -e PGDATA=/var/lib/postgresql/data/pgdata -v $target_dir:/var/lib/postgresql/data -v $conf_dir/postgres.conf:/etc/postgresql/postgresql.conf -p 5432:5432 postgres -c 'config_file=/etc/postgresql/postgresql.conf'
+docker run -d --name pek-postgres -h pek-postgres \
+           -e POSTGRES_PASSWORD=*** \
+           -e PGDATA=/var/lib/postgresql/data/pgdata \
+           -v $target_dir:/var/lib/postgresql/data \
+           -v $conf_dir/postgres.conf:/etc/postgresql/postgresql.conf \
+           -p 5432:5432 \
+           postgres -c 'config_file=/etc/postgresql/postgresql.conf'
