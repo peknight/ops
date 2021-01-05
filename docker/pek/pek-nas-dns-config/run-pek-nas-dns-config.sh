836c0b10ed3f72f776f1a3885e18cbb3b02032b7
peknight <JKpeknight@gmail.com>
2021-01-05 20:54:03 +0800

    更新pek-nas-dns-config

--- docker/pek/pek-nas-dns-config/run-pek-nas-dns-config.sh
+++ docker/pek/pek-nas-dns-config/run-pek-nas-dns-config.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d --restart always --name pek-nas-dns-config -h pek-nas-dns-config pek-nas-dns-config:0.1-SNAPSHOT
+docker run -d --restart always --name pek-nas-dns-config -h pek-nas-dns-config --net host --cap-add NET_ADMIN pek-nas-dns-config:0.1-SNAPSHOT
