cfee4119c690eb8688c596e09e0389542c0f8eb7
peknight <JKpeknight@gmail.com>
2021-01-30 18:40:02 +0800

    update run-aria2-pt.sh

--- docker/aria2/pt/run-aria2-pt.sh
+++ docker/aria2/pt/run-aria2-pt.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-target_dir=$HOME/software/aria2/pt/data
-if [ ! -d "$target_dir" ]; then
-  mkdir -p $target_dir
+data_dir=$HOME/software/aria2/pt/data
+if [ ! -d "$data_dir" ]; then
+  mkdir -p $data_dir
 fi
-conf_dir=$HOME/software/aria2/pt/etc
+conf_dir=$HOME/software/aria2/pt/conf
 if [ ! -d "$conf_dir" ]; then
   mkdir -p $conf_dir
 fi
@@ -16,4 +16,4 @@ if [ ! -e "$conf_dir/aria2.session" ]; then
 fi
 tracker=$(curl -sSL 'https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best.txt' | sed '/^\s*$/d' | tee tracker | tr '\n' ',' | sed 's/,$//')
 sed -i "s!^\(bt-tracker=\).*!\1${tracker}!;s/pek/$(id -nu)/g" $conf_dir/aria2.conf
-docker run -d --name pek-aria2-pt -h pek-aria2-pt -v $conf_dir:/home/$(id -nu)/.aria2 -v $target_dir:/data --net host --cap-add NET_ADMIN pek/webui-aria2-pt
+docker run -d --name pek-aria2-pt -h pek-aria2-pt -v $conf_dir:/home/$(id -nu)/.aria2 -v $data_dir:/data --net host --cap-add NET_ADMIN pek/webui-aria2-pt
