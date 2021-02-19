2a9c72a8e69464e114a1b2896d2085c38f1ace64
peknight <JKpeknight@gmail.com>
2021-02-19 15:02:55 +0800

    update nginx file

--- docker/nginx/file/run-nginx.sh
+++ docker/nginx/file/run-nginx.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-target_dir=$HOME/software/nginx/file/data
-if [ ! -d "$target_dir" ]; then
-  mkdir -p $target_dir
+data_dir=$HOME/software/nginx/file/data
+if [ ! -d "$data_dir" ]; then
+  mkdir -p $data_dir
 fi
-conf_dir=$HOME/software/nginx/file/etc
+conf_dir=$HOME/software/nginx/file/conf
 if [ ! -d "$conf_dir" ]; then
   mkdir -p $conf_dir
 fi
@@ -11,4 +11,4 @@ fi
 if [ ! -e "$conf_dir/nginx.conf" ]; then
   cp nginx.conf $conf_dir
 fi
-docker run -d --name pek-file -h pek-file -v $target_dir:/data/file -v $conf_dir/nginx.conf:/etc/nginx/nginx.conf:ro -p 33101:33101 nginx:stable-alpine
+docker run -d --name pek-file -h pek-file -v $data_dir:/data/file -v $conf_dir/nginx.conf:/etc/nginx/nginx.conf:ro -p 33101:33101 nginx:stable-alpine
