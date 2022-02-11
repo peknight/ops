1e31afb1a409ab9222a4fba601b219a8d8f8cbff
peknight <JKpeknight@gmail.com>
2022-02-11 16:43:23 +0800

    update frp

--- docker/frp/server/run-frps.sh
+++ docker/frp/server/run-frps.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-conf_dir=$HOME/software/frp/server/etc
+conf_dir=$HOME/software/frp/server/conf
 if [ ! -d "$conf_dir" ]; then
   mkdir -p $conf_dir
 fi
 if [ ! -e "$conf_dir/frps.ini" ]; then
   cp frps.ini $conf_dir/
 fi
-docker run -d --name pek-frps -h pek-frps -v $conf_dir:/conf -p 7000:7000 -p 31001-31100:31001-31100 pek/frps:0.27.0
+docker run -d --name pek-frps -h pek-frps -v $conf_dir:/conf -p 7000:7000 -p 31001-31100:31001-31100 pek/frps:0.39.1
