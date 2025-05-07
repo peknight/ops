40378de840b72c235008aa344ed1e6aa47aa4622
peknight <JKpeknight@gmail.com>
2025-05-07 13:50:36 +0800

    update

--- docker/frp/client/run-frpc.sh
+++ docker/frp/client/run-frpc.sh
@@ -3,7 +3,7 @@ conf_dir=$HOME/software/frp/client/conf
 if [ ! -d "$conf_dir" ]; then
   mkdir -p $conf_dir
 fi
-if [ ! -e "$conf_dir/frpc.ini" ]; then
+if [ ! -e "$conf_dir/frpc.toml" ]; then
   cp frpc.toml $conf_dir/
 fi
 docker run -d --name pek-frpc -h pek-frpc -v $conf_dir:/conf pek/frpc:0.62.1
