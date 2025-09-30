ba41a5f39fc7dfc520b65f90c73eb3ee2d6cde36
peknight <JKpeknight@gmail.com>
2025-09-30 16:48:16 +0800

    update

---
+++ docker/minecraft/bedrock/1.21.102.1/src/build-mc-bedrock-server-image.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+wget "https://www.minecraft.net/bedrockdedicatedserver/bin-linux/bedrock-server-1.21.102.1.zip"
+unzip bedrock-server-1.21.102.1.zip -d bedrock-server
+cd bedrock-server
+tar -cf bedrock-server.tar *
+cd ../
+mv bedrock-server/bedrock-server.tar ./
+docker build -t pek/minecraft-bedrock-server:1.21.102.1 .
