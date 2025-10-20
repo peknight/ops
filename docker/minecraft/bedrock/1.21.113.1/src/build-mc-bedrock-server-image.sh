e9f80dc530437b25864738169e3e0c208acdc6d7
peknight <JKpeknight@gmail.com>
2025-10-20 13:58:17 +0800

    update

--- docker/minecraft/bedrock/1.21.113.1/src/build-mc-bedrock-server-image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-wget "https://www.minecraft.net/bedrockdedicatedserver/bin-linux/bedrock-server-1.21.113.1.zip"
-unzip bedrock-server-1.21.113.1.zip -d bedrock-server
-cd bedrock-server
-tar -cf bedrock-server.tar *
-cd ../
-mv bedrock-server/bedrock-server.tar ./
-docker build -t pek/minecraft-bedrock-server:1.21.113.1 .
--- docker/minecraft/bedrock/run-mc-bedrock-server.sh
+++ docker/minecraft/bedrock/run-mc-bedrock-server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-version=1.21.113.1
+version=1.21.114.1
 while getopts :v: opt
 do
     case "$opt" in
