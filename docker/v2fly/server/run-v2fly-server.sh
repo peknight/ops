12554607e68f82ef1b95cd1d2445e02d403ebfa9
peknight <JKpeknight@gmail.com>
2026-01-19 05:25:02 +0000

    update

--- docker/v2fly/server/run-v2fly-server.sh
+++ docker/v2fly/server/run-v2fly-server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-conf_dir=$HOME/software/v2fly/server/conf
+conf_dir=$HOME/opt/v2ray/server/conf
 if [ ! -d "$conf_dir" ]; then
   mkdir -p $conf_dir
 fi
@@ -8,16 +8,16 @@ if [ ! -e "$conf_dir/config.json" ]; then
   UUID=$(cat /proc/sys/kernel/random/uuid)
   sed -i "s/60ca58e9-003e-4c01-98de-c2223ae49153/$UUID/g" $conf_dir/config.json
 fi
-#cert_dir=$HOME/software/v2fly/server/cert
-#if [ ! -e "$cert_dir/v2fly.crt" ]; then
-#  echo "copy v2fly.crt to ${cert_dir}"
+#cert_dir=$HOME/software/v2ray/server/cert
+#if [ ! -e "$cert_dir/v2ray.crt" ]; then
+#  echo "copy v2ray.crt to ${cert_dir}"
 #  exit 1
 #fi
-#if [ ! -e "$cert_dir/v2fly.key" ]; then
-#  echo "copy v2fly.key to ${cert_dir}"
+#if [ ! -e "$cert_dir/v2ray.key" ]; then
+#  echo "copy v2ray.key to ${cert_dir}"
 #  exit 1
 #fi
-log_dir=$HOME/software/v2fly/server/log
+log_dir=$HOME/software/v2ray/server/log
 if [ ! -d "$log_dir" ]; then
   mkdir -p $log_dir
 fi
@@ -27,10 +27,10 @@ fi
 if [ ! -e "$log_dir/error.log" ]; then
   touch $log_dir/error.log
 fi
-docker run -d --restart always --name pek-v2fly-server -h pek-v2fly-server \
+docker run -d --restart unless-stopped --name pek-v2ray-server -h pek-v2ray-server \
            -e V2RAY_VMESS_AEAD_FORCED=false \
-           -v $conf_dir/config.json:/etc/v2fly/config.json \
-           -v $log_dir:/var/log/v2fly \
-           --net host --cap-add NET_ADMIN \
-           v2fly/v2fly-core:v5.30.0 \
-           run -c /etc/v2fly/config.json
+           -v $conf_dir/config.json:/etc/v2ray/config.json \
+           -v $log_dir:/var/log/v2ray \
+           --net host \
+           v2fly/v2fly-core:v5.41.0 \
+           run -c /etc/v2ray/config.json
