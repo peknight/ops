212450ee8afe648cb0e59de856899a008b790f96
peknight <JKpeknight@gmail.com>
2026-02-05 18:49:30 +0800

    移除frp

--- docker/frp/client/run-frpc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-conf_dir=$HOME/software/frp/client/conf
-if [ ! -d "$conf_dir" ]; then
-  mkdir -p $conf_dir
-fi
-if [ ! -e "$conf_dir/frpc.toml" ]; then
-  cp frpc.toml $conf_dir/
-fi
-docker run -d --restart always --name pek-frpc -h pek-frpc --network pek-network -v $conf_dir:/conf pek/frpc:0.66.0
